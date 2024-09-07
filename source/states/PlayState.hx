2cfb26ab915324fc6819745bce92b57f5997b742
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-07 10:23:32 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -371,6 +371,8 @@ class PlayState extends MusicBeatState
 		luaDebugGroup.cameras = [camOther];
 		add(luaDebugGroup);
 		#end
+				
+		var speakers:FlxSprite;
 
 		if (!stageData.hide_girlfriend)
 		{
@@ -379,6 +381,13 @@ class PlayState extends MusicBeatState
 			startCharacterPos(gf);
 			gfGroup.scrollFactor.set(0.95, 0.95);
 			gfGroup.add(gf);
+			
+		        if (SONG.player1 == 'bf-eevee')
+			SONG.gfVersion = 'gf-vulpix';
+			speakers.frames = Paths.getSparrowAtlas('SpeakersOnly');
+     			speakers.setPosition(GF_X, GF_Y);
+			speakers.animation.addByIndices('danceLeft', 'GF Dancing Beat0', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+			speakers.animation.addByIndices('danceRight', 'GF Dancing Beat0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		}
 
 		dad = new Character(0, 0, SONG.player2);
